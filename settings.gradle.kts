ef675ee7dc54718d77fe507fb1dea36bf0cc0f30
drile <driley@csumb.edu>
2023-12-02 23:16:14 -0800

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CST338FA23_Project2_LibraryApplication_Final"
+include(":app")
+ 
