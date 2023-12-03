fb43f0fafe1c41148fa69ac6bb9a51ca52afe6e9
drile <driley@csumb.edu>
2023-12-02 23:40:37 -0800

    Updated LoginActivity - Created Fields: mUsernameField, mPasswordField, mUsername, mPassword, mLoginButton, mUserDAO, mUser, mTestNormUser, mTestAdminUser

--- app/src/main/java/com/example/cst338fa23_project2_libraryapplication_final/LoginActivity.java
+++ app/src/main/java/com/example/cst338fa23_project2_libraryapplication_final/LoginActivity.java
@@ -3,10 +3,21 @@ package com.example.cst338fa23_project2_libraryapplication_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.cst338fa23_project2_libraryapplication_final.DB.User;
+import com.example.cst338fa23_project2_libraryapplication_final.DB.UserDAO;
 import com.example.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mUsernameField, mPasswordField;
+    private String mUsername, mPassword;
+    private Button mLoginButton;
+    private UserDAO mUserDAO;
+    private User mUser;
+    private User mTestNormUser = new User("testuser1", "testuser1", false);
+    private User mTestAdminUser = new User("admin2", "admin2", true);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
