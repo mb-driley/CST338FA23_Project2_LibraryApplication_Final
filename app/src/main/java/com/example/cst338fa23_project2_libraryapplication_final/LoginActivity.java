2774fc34db1e906624565e44a98a966f71046e62
drile <driley@csumb.edu>
2023-12-02 23:36:55 -0800

    Created activity_login & LoginActivity activity_login: - Created the Layout for the Page
    Updated AndroidManifest & strings
    AndroidManifest:
    - Updated to make LoginActivity the default starting page
    
    strings:
    - Created new string resources from activity_login.xml

---
+++ app/src/main/java/com/example/cst338fa23_project2_libraryapplication_final/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.cst338fa23_project2_libraryapplication_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
